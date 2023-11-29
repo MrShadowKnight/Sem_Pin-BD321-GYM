6c7dfab5d41686e9af263d2a44090ee14add356b
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2023-11-29 18:46:17 +0200

    Add.index

---
+++ src/layouts/header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <header className={s.header}>
+        <h1>Header</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+//layout
+import Layout from "@/layouts";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
