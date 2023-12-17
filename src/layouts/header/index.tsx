f3d33be97ba6a103e10ac3ca327ed57a9a95e1eb
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2023-12-17 18:49:11 +0200

    Add.changes

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -2,32 +2,32 @@ import Link from "next/link";
 import s from "./header.module.scss";
 
 const Header = () => {
-	return (
-		<>
-			<header className={s.header}>
-				<div className={s.header__icon}>
-					<img src="dumpbell2.svg" alt="" />
-					<p>ANGEL FIT</p>
-				</div>
-				<div></div>
-				<div></div>
-				<Link className={s.header__link} href={`/`}>
-					HOME
-				</Link>
-				<Link className={s.header__link} href={`/coaches`}>
-					COACHES
-				</Link>
-				<Link className={s.header__link} href={`/`}>
-					ARTICLES
-				</Link>
-				<div></div>
-				<div></div>
-				<Link className={s.signUp__link} href={`/`}>
-					GET STARTED FOR FREE
-				</Link>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className={s.header}>
+        <div className={s.header__icon}>
+          <img src="dumpbell2.svg" alt="" />
+          <p>ANGEL FIT</p>
+        </div>
+        <div></div>
+        <div></div>
+        <Link className={s.header__link} href={`/`}>
+          HOME
+        </Link>
+        <Link className={s.header__link} href={`/coaches`}>
+          COACHES
+        </Link>
+        <Link className={s.header__link} href={`/`}>
+          ARTICLES
+        </Link>
+        <div></div>
+        <div></div>
+        <Link className={s.signUp__link} href={`/`}>
+          GET STARTED FOR FREE
+        </Link>
+      </header>
+    </>
+  );
 };
 
 export default Header;
