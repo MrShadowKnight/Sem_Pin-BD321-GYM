44c1f29644fae38f1139968bbb74fcd14e01efe0
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2024-01-21 16:09:48 +0200

    Add.index

--- src/components/about.home/index.tsx
+++ src/components/about.home/index.tsx
@@ -6,26 +6,48 @@ import Image from "next/image";
 // import components
 import Card from "../card.about/index";
 
+// import images
+import homeGym from "@/assets/images/home-design.jpg";
+import arrow from "@/assets/images/arrow.png";
+
 const About = () => {
   return (
     <>
       <section className={s.about}>
         <div className={s.about__nulla}>
-          <h3 className={s.about__subtitle}>ABOUT US</h3>
-          <h1 className={s.about__title}>
+          <h3 className={s.about__nulla_subtitle}>ABOUT US</h3>
+          <h1 className={s.about__nulla_title}>
             Nulla id
             <br />
             accumsan erat
           </h1>
-          <h4 className={s.about__text}>
+          <h4 className={s.about__nulla_text}>
             Lorem ipsum dolor sit amet, consetetur .
           </h4>
-          <div className={s.about__grid}>
+          <div className={s.about__nulla_grid}>
             <Card />
             <Card />
           </div>
         </div>
-        <div className={s.about__box2}></div>
+        <div className={s.about__homeGym}>
+          <div className={s.about__homeGym_background}>
+            <Image src={homeGym} alt="" />
+          </div>
+          <div className={s.about__homeGym_text}>
+            <h1 className={s.about__homeGym_text_title}>
+              WANY TO PRACTICE <br />
+              AT HOME ?
+            </h1>
+            <Link href={`/`} className={s.about__homeGym_text_link}>
+              <p className={s.about__homeGym_text_link_text}>CLICK HERE</p>
+              <Image
+                className={s.about__homeGym_text_link_img}
+                src={arrow}
+                alt=""
+              />
+            </Link>
+          </div>
+        </div>
       </section>
     </>
   );
